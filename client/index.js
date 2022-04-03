27643c93f3b3c2ad72b8cb9e7385d420c8755f0c
827094438@qq.com <827094438@qq.com>
2022-04-03 22:01:24 +0800

    feat: xintiao

--- client/index.js
+++ client/index.js
@@ -1,35 +1,21 @@
-var ws = new WebSocket("ws://localhost:8083");
-ws.onclose = function () {
-  console.log("ws onclose");
-  clearTimeout(heartCheck.timeoutObj);
-};
-ws.onopen = function () { 
-  console.log("ws onopen");
-  ws.send("from client: hello");
-  heartCheck.start();
-};
-ws.onmessage = function (e) {
-  console.log("from server: " + e.data);
-  heartCheck.reset();
-};
+const WebSocket = require("ws");
 
-window.onload = () => {
-  const buttonEl = document.getElementById("button");
-  buttonEl.addEventListener("click", () => {
-    ws.send(Math.random());
-  });
-};
+function heartbeat() {
+  clearTimeout(this.pingTimeout);
+  console.log("13221", 121111);
+  // Use `WebSocket#terminate()`, which immediately destroys the connection,
+  // instead of `WebSocket#close()`, which waits for the close timer.
+  // Delay should be equal to the interval at which your server
+  // sends out pings plus a conservative assumption of the latency.
+  this.pingTimeout = setTimeout(() => {
+    this.terminate();
+  }, 3000 + 1000);
+}
 
-var heartCheck = {
-  timeout: 3000,
-  timeoutObj: null,
-  reset: function () {
-    clearTimeout(this.timeoutObj);
-    this.start();
-  },
-  start: function () {
-    this.timeoutObj = setTimeout(function () {
-      ws.send(JSON.stringify({ type: "HeartBeat" }));
-    }, this.timeout);
-  },
-};
+const client = new WebSocket("ws://localhost:8083");
+
+client.on("open", heartbeat);
+client.on("ping", heartbeat);
+client.on("close", function clear() {
+  clearTimeout(this.pingTimeout);
+});
--- index.js
+++ index.js
@@ -5,24 +5,33 @@ const app = express();
 const server = require("http").Server(app);
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8083 });
+const wss = new WebSocket.WebSocketServer({ port: 8083 });
 
 wss.on("connection", function connection(ws) {
   console.log("server: receive connection.");
 
-  ws.on("message", function incoming(data) {
-    console.log("data", typeof data.toString());
-    console.log("JSON", data.toString());
-    if (data.toString().includes("type")) {
-      console.log(JSON.parse(data.toString()));
-      const formatData = JSON.parse(data.toString());
-      if (formatData.type === "HeartBeat") {
-        ws.send(JSON.stringify({ type: "HeartBeat---server " }));
-      }
-    }
-  });
+  ws.isAlive = true;
+  ws.on("pong", heartbeat);
 
   ws.send("world");
+});
+function heartbeat(...ws) {
+  this.isAlive = true;
+  console.log("xintiao");
+  console.log(111, ws);
+}
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping();
+  });
+}, 3000);
+
+wss.on("close", function close() {
+  console.log("关闭");
+  clearInterval(interval);
 });
 
 app.get("/", function (req, res) {
